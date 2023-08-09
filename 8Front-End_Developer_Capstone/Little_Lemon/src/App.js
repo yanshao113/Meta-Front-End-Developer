32490f07e43b322d95234144aead00c43b076171
yanshao113 <139802247+yanshao113@users.noreply.github.com>
2023-08-08 21:00:15 -0700

    Add files via upload

---
+++ 8Front-End_Developer_Capstone/Little_Lemon/src/App.js
@@ -0,0 +1,27 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/Header";
+import AboutSection from "./components/AboutSection";
+import MenuSection from "./components/MenuSection";
+import ContactMeSection from "./components/ContactMeSection";
+import Footer from "./components/Footer";
+import { AlertProvider } from "./context/alertContext";
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <AlertProvider>
+        <main>
+          <Header />
+          <AboutSection />
+          <MenuSection />
+          <ContactMeSection />
+          <Footer />
+          <Alert />
+        </main>
+      </AlertProvider>
+    </ChakraProvider>
+  );
+}
+
+export default App;
